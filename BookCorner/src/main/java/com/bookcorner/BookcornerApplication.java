73a869febea80fdc6ed167fd71d2cc494222adc9
Angela <angela.stevkovska3@gmail.com>
2022-02-27 12:56:59 +0100

    Application init

---
+++ BookCorner/src/main/java/com/bookcorner/BookcornerApplication.java
@@ -0,0 +1,13 @@
+package com.bookcorner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcornerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookcornerApplication.class, args);
+    }
+
+}
---
+++ BookCorner/src/test/java/com/bookcorner/BookcornerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookcorner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookcornerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
