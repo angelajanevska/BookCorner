b2db34ae0d47bb518406b96c86d60212d94fdf40
Angela <angela.stevkovska3@gmail.com>
2022-03-01 13:52:25 +0100

    template added

--- BookCorner/src/main/java/com/bookcorner/BookcornerApplication.java
+++ BookCorner/src/main/java/com/bookcorner/BookcornerApplication.java
@@ -2,8 +2,10 @@ package com.bookcorner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class BookcornerApplication {
 
     public static void main(String[] args) {
